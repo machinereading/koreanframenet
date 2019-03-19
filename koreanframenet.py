d4f59c3a499bd948e72a798fa99710fcb5a1aec2
hahmyg <hahmyg@gmail.com>
2019-03-19 14:52:36 +0900

    1.0

---
+++ koreanframenet.py
@@ -0,0 +1,87 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+import json
+import os
+from .src import dataio
+
+
+# In[2]:
+
+
+print('### Korean FrameNet ###')
+print('\t# contact: hahmyg@kaist, hahmyg@gmail.com #')
+print('')
+
+
+# In[11]:
+
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+
+# In[19]:
+
+
+class interface():
+    def __init__(self, version=1.0):
+        self.version = str(version)
+        file_path = dir_path+'/resource/'+self.version+'/'
+        with open(file_path+'KFN_lus.json', 'r') as f:
+            self.kfn_lus = json.load(f)
+        with open(file_path+'KFN_annotations.json', 'r') as f:
+            self.kfn_annos = json.load(f)
+            
+    def load_data(self):
+        trn, dev, tst = dataio.load_framenet_data(self.version)
+        return trn, dev, tst
+    
+    def lus_by_word(self, word):
+        result = []
+        for luid in self.kfn_lus:
+            d = self.kfn_lus[luid]
+            if word == d['lexeme']:
+                item = {}
+                item['lu'] = d['lexeme']+'.'+d['pos']
+                item['frame'] = d['frame']
+                item['lu_id'] = int(luid)
+                result.append(item)
+        return result
+    
+    def lus_by_token(self, token):
+        result = []
+        for luid in self.kfn_lus:
+            d = self.kfn_lus[luid]
+            if token in d['surface_forms']:
+                item = {}
+                item['lu'] = d['lexeme']+'.'+d['pos']
+                item['frame'] = d['frame']
+                item['lu_id'] = int(luid)
+                result.append(item)
+        return result
+        
+    
+    def annotations_by_lu(self, luid):
+        result = []
+        luid = str(luid)
+        anno_ids = self.kfn_lus[luid]['annotation_ids']
+        for anno_id in anno_ids:
+            anno_id = str(anno_id)
+            anno = self.kfn_annos[anno_id]
+            args = []
+            for a in anno['denotations']:
+                if a['role'] == 'ARGUMENT':
+                    arg = a['text'] + ' ['+a['obj']+']'
+                    args.append(arg)
+            item = {}
+            item['lu'] = anno['lu']
+            item['text'] = anno['text']
+            item['arguments'] = args
+            result.append(item)
+        return result
+    
+    
+
---
+++ src/dataio.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+def lines2tsv(lines):
+    tsv = []
+    sent = []
+    for line in lines:
+        line = line.strip()
+        if line.startswith('#'):
+            pass
+        else:
+            if line != '':
+                token = line.split('\t')
+                sent.append(token)
+            else:
+                tsv.append(sent)
+                sent = []
+    return tsv
+
+def conll2data(conll):
+    tsv = lines2tsv(conll)    
+    data = []
+    for sent in tsv:     
+        tok_str, tok_lu, tok_frame, tok_fe= [],[],[],[]
+        for token in sent:
+            tok_str.append(token[1])
+            tok_lu.append(token[2])
+            tok_frame.append(token[3])
+            if token[4].startswith('S'):
+                token[4] = token[4].replace('S-','B-')
+            tok_fe.append(token[4])
+        sent_list = []
+        sent_list.append(tok_str)
+        sent_list.append(tok_lu)
+        sent_list.append(tok_frame)
+        sent_list.append(tok_fe)
+        data.append(sent_list)
+    return data     
+
+def load_framenet_data(version):
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    fname = dir_path+'/../data/'+version+'/'
+    with open(fname+'training.tsv','r') as f:
+        trn_data = f.readlines()
+    with open(fname+'dev.tsv','r') as f:
+        dev_data = f.readlines()
+    with open(fname+'test.tsv','r') as f:
+        tst_data = f.readlines()
+        
+    trn = conll2data(trn_data)
+    dev = conll2data(dev_data)
+    tst = conll2data(tst_data)
+    
+    print('\n### loading Korean FrameNet', version, 'data...')
+    print('\t# of instances in training data:', len(trn))
+    print('\t# of instances in dev data:', len(dev))
+    print('\t# of instances in test data:', len(tst))
+    
+    return trn, dev, tst
